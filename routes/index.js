e69fe6efc54938e76de87b3715bd20367bc60069
kayodegigz <kayogunrinde@gmail.com>
2023-06-04 03:04:36 +0100

    set content type to json

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 
 const router = (app) => {
   const route = express.Router();
+  app.use(express.json());
   app.use('/', route);
 
   route.get('/status', (req, res) => AppController.getStatus(req, res));
