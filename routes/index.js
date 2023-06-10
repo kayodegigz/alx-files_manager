61a47881d1434bab1ee0ec21a80976ef946a5625
kayodegigz <kayogunrinde@gmail.com>
2023-06-10 08:52:16 +0100

    correct endpoint spelling error

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,6 @@ const router = (app) => {
   route.get('/files/:id', (req, res) => FilesController.getShow(req, res));
   route.get('/files', (req, res) => FilesController.getIndex(req, res));
   route.put('/files/:id/publish', (req, res) => FilesController.putPublish(req, res));
-  route.put('/files/:id/publish', (req, res) => FilesController.putUnpublish(req, res));
+  route.put('/files/:id/unpublish', (req, res) => FilesController.putUnpublish(req, res));
 };
 export default router;
